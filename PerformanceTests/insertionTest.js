7afa7ad225f4f94027ff197aa14e33e2cebce8b6
John Doty <jrhdoty@gmail.com>
2014-06-21 23:21:07 -0700

    clean up testing and append results to DOM

--- PerformanceTests/insertionTest.js
+++ PerformanceTests/insertionTest.js
@@ -2,12 +2,13 @@
 
 function benchmark() {
 
-  function test(count) {
+  function test(numPoints) {
     var max = 10000;  //dimensions of area
     var p = []; //list of points
-    var x, y;
-    //create 'count' many points with random coordinates in area
-    for(var i = 0; i < count; i++){
+    var x, y, i;
+    
+    //create 'numPoints' many points with random coordinates in area
+    for( i = 0; i < numPoints; i++ ){
       x = Math.random()*max;
       y = Math.random()*max;
       p.push(new Point(x, y));
@@ -17,17 +18,17 @@ function benchmark() {
     var box = new Box(new Point(0, 0), new Point(max, max));
     var quad = new Quadtree(box);
 
-    var start = +new Date;  //set start time for test
+    var start = +new Date();  //set start time for test
 
     //insert all of the points
-    var i = count;
+    i = numPoints;
     while (i > 0) {
       quad.insert(p[i-1], true);
       i--;
     }
 
     //do a queryRange lookup on all of the points in a 20x20 box with the point in the middle
-    i = count;
+    i = numPoints;
     while(i > 0){
       quad.queryRange(new Box(new Point(p[i-1].x-10, p[i-1].y-10), 
                               new Point(p[i-1].x+10, p[i-1].y+10)));
@@ -35,15 +36,20 @@ function benchmark() {
     }
 
     //set end time for the test
-    var end = +new Date;
+    var end = +new Date();
     //translate into seconds and return
     return ((end - start)/1000.0);
   }
 
+  var t, p, output;
   //print results of each test
-  for (var i = 1000; i <= 21000; i += 10000) {
+  for (var i = 10000; i <= 100000; i += 10000) {
     t = test(i);
-    console.log(i + ' insertions: ' + t + ' /sec');
+    output = i + ' insertions: ' + t + ' /sec';
+    console.log(output);
+    p = document.createElement('p');
+    p.innerHTML = output;
+    document.body.appendChild(p);
   }
 }
 
